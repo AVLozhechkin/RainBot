afdc0650afb581800c736f78de1ff175c95875a2
Artem <avlozhechkin@gmail.com>
2023-07-26 23:56:36 +0300

    Adds zip removal after deployment

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -29,12 +29,23 @@ module "rainbot" {
   yandex_weather_api_key = var.yandex_weather_api_key
 }
 
-resource "null_resource" "set-webhooks-and-create-database" {
+resource "null_resource" "set-webhooks" {
+  depends_on = [module.rainbot]
+  provisioner "local-exec" {
+    command = "curl -F url=https://${module.rainbot.api_gateway_webhook_endpoint} https://api.telegram.org/bot${var.telegram_bot_token}/setWebhook"
+  }
+}
+
+resource "null_resource" "create-database-tables" {
   depends_on = [module.rainbot]
   provisioner "local-exec" {
     command = "python ../create_tables.py -d ${module.rainbot.yandex_database_path} -t ${var.iam_token} -e ${module.rainbot.yandex_database_endpoint}"
   }
+}
+
+resource "null_resource" "delete-zips" {
+  depends_on = [module.rainbot]
   provisioner "local-exec" {
-    command = "curl -F url=https://${module.rainbot.api_gateway_webhook_endpoint} https://api.telegram.org/bot${var.telegram_bot_token}/setWebhook"
+    command = "rm -r ../zips"
   }
 }
