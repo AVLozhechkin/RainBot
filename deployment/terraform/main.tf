5e87d862920e4bc18c6a1f0b66714213b91c53a6
Artem <avlozhechkin@gmail.com>
2023-07-30 17:46:00 +0300

    Updates required_providers

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -12,6 +12,14 @@ terraform {
     yandex = {
       source = "yandex-cloud/yandex"
     }
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.1"
+    }
   }
   required_version = ">= 0.13"
 }
